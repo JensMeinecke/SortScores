42c5660480e24b51bf9a179609a4c601477847d3
JensMeinecke <jens@meinecke.net.au>
2016-03-15 14:44:33 +1000

    Split out Unit Tests

--- UnitTests.cs
+++ UnitTests.cs
@@ -6,19 +6,34 @@ namespace SortScores
     public class UnitTests
     {
         [TestMethod]
-        public void TestResultCreation()
+        public void TestResultInvalidInteger()
+        {
+            var nullResult = TestResult.FromString("BUNDY, TERESSA, 88.98");
+            Assert.IsNull(nullResult, "Should have failed on too bad integer for score");
+        }
+
+        [TestMethod]
+        public void TestResultTooManyColumns()
+        {
+            var nullResult = TestResult.FromString("BUNDY, TERESSA, 88, 98");
+            Assert.IsNull(nullResult, "Should have failed on too many columns");
+        }
+
+        [TestMethod]
+        public void TestResultTooFewColumns()
+        {
+            var nullResult = TestResult.FromString("BUNDY, 88");
+            Assert.IsNull(nullResult, "Should have failed on too few columns");
+        }
+
+        [TestMethod]
+        public void TestResultProperParsing()
         {
             var testResult = TestResult.FromString("BUNDY, TERESSA, 88");
 
             Assert.AreEqual(testResult.FirstName, "TERESSA");
             Assert.AreEqual(testResult.LastName, "BUNDY");
             Assert.AreEqual(testResult.Score, 88);
-
-            var nullResult = TestResult.FromString("BUNDY, TERESSA, 88, 98");
-            Assert.IsNull(nullResult, "Should have failed on too many columns");
-
-            nullResult = TestResult.FromString("BUNDY, TERESSA, 88.98");
-            Assert.IsNull(nullResult, "Should have failed on too bad integer for score");
         }
     }
 }
