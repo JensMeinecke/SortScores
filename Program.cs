fd3fbab313cb0603d68670fa792b37a73f184188
JensMeinecke <jens@meinecke.net.au>
2016-03-15 14:01:20 +1000

    Version 1.0.0.0

--- Program.cs
+++ Program.cs
@@ -1,15 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SortScores
 {
     class Program
     {
+        private static void DisplayUsage()
+        {
+             Console.WriteLine(@"Expected one argument for input file
+
+     SortScores fileName
+");
+        }
+
         static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                DisplayUsage();
+                return;
+            }
+
+            string fileName = args[0];
+
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine($"File '{fileName}' not found");
+                return;
+            }
+
+            string[] lines;
+
+            try
+            {
+                lines = File.ReadAllLines(fileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unable to read File '{fileName}' : {ex.Message}");
+                return;
+
+            }
+
+            List<TestResult> results = new List<TestResult>();
+
+            int errors = 0;
+
+            foreach (var line in lines)
+            {
+                string text = line.Trim();
+
+                if (string.IsNullOrEmpty(text))
+                    continue; // ignore blank lines
+
+                var result = TestResult.FromString(text);
+
+                if (result == null)
+                {
+                    Console.WriteLine($"Unable to parse line: '{line}'");
+                    errors++;
+                    continue;
+                }
+
+                results.Add(result);
+            }
+
+            var outputName = Path.GetFileNameWithoutExtension(fileName) + "-sorted.txt";
+
+            try
+            {
+                using (TextWriter tw = new StreamWriter(outputName))
+                {
+                    results.Sort(TestResult.Compare);
+
+                    foreach (var result in results)
+                    {
+                        string line = $"{result.LastName}, {result.FirstName}, {result.Score}";
+                        Console.WriteLine(line);
+                        tw.WriteLine(line);
+                    }
+                }
+
+                if (errors > 0)
+                    Console.WriteLine($"Encountered {errors} errors");
+
+                Console.WriteLine($"Finished: created {outputName}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unable to write to output file '{fileName}' : {ex.Message}");
+            }
+
         }
     }
 }
---
+++ TestResult.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SortScores
+{
+    internal class TestResult
+    {
+        public int Score { get; }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+
+        private static class Column
+        {
+            public const int LastName = 0;
+            public const int FirstName = 1;
+            public const int Score = 2;
+
+            public const int MinColumns = Score + 1;
+        }
+
+        private TestResult(string lastName, string firstName, int score)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Score = score;
+        }
+
+        public static TestResult FromString(string source)
+        {
+            var data = source.Split(',');
+
+            if (data.Length != Column.MinColumns)
+                return null;
+
+            int score;
+
+            if (!Int32.TryParse(data[Column.Score].Trim(), out score) || score < 0)
+                return null;
+
+            return new TestResult(data[Column.LastName].Trim(), data[Column.FirstName].Trim(), score);
+        }
+
+        public static int Compare(TestResult t1, TestResult t2)
+        {
+            int diff = t2.Score - t1.Score;
+
+            if (diff != 0)
+                return diff;
+
+            diff = string.Compare(t1.LastName, t2.LastName);
+
+            if (diff != 0)
+                return diff;
+
+            return string.Compare(t1.LastName, t2.LastName);
+        }
+    }
+}
